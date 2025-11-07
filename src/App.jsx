605bfeec4e55445b08f005c91aadccbdc50d4e05
aitheron-Co <admin@aitheron.gr>
2025-11-07 12:33:54 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
 import { useEffect, useState } from "react";
 import PartnershipsSection from "./components/PartnershipsSection.jsx";
+import AzureServicesSection from "./components/AzureServicesSection.jsx";
 
 
 export default function AitheronSite() {
@@ -308,6 +309,9 @@ useEffect(() => {
       </section>
 
       <PartnershipsSection />
+
+      {/* Azure & Microsoft Services */}
+      <AzureServicesSection />
 
       {/* Industries */}
       <section id="industries" className="border-t border-white/10">
