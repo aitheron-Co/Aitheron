c01a1b71c25474207f0818fdf637939f922b37c0
aitheron-Co <admin@aitheron.gr>
2025-11-07 13:23:57 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AitheronSite from "./App.jsx";
+import AzureServicesPage from "./pages/AzureServicesPage.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AitheronSite />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AitheronSite />} />
+        <Route path="/services/azure" element={<AzureServicesPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
