8ecbeebffb5874c3101473d280f6dff119ada88b
aitheron-Co <admin@aitheron.gr>
2025-11-10 17:56:19 +0200

    Update useClarityPageView.js

--- src/hooks/useClarityPageView.js
+++ src/hooks/useClarityPageView.js
@@ -1,1 +1,12 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
+export default function useClarityPageView() {
+  const { pathname, search, hash } = useLocation();
+
+  useEffect(() => {
+    if (window.clarity) {
+      window.clarity("event", "pageview");
+    }
+  }, [pathname, search, hash]);
+}
