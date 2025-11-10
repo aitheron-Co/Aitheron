f520dd3d6054d800568783fec98e7ffaea880920
aitheron-Co <admin@aitheron.gr>
2025-11-10 18:22:45 +0200

    Update loadClarity.js

--- src/utils/loadClarity.js
+++ src/utils/loadClarity.js
@@ -1,1 +1,11 @@
+// src/utils/loadClarity.js
+export function loadClarityOnce(projectId) {
+  if (document.getElementById("clarity-script")) return;
+  const s = document.createElement("script");
+  s.id = "clarity-script";
+  s.type = "text/javascript";
+  s.async = true;
+  s.src = "https://www.clarity.ms/tag/" + projectId;
+  document.head.appendChild(s);
+}
 
